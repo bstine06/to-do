c0e9222aa7790546cfe3fdcd9a355f481c9a9681
Brett Stine <r.brettstine@gmail.com>
2024-06-18 16:53:05 -0400

    Create View class to handle all DOM manipulation
     - renamed TodoItem to "Task"

--- src/index.js
+++ src/index.js
@@ -1,176 +1,47 @@
 import './style.css';
-import { TodoItem } from './todo-item.js';
+import { Task } from './task.js';
+import { View } from './view.js';
 
 const projects = {};
+
+const view = new View();
+
+view.setupNewTaskModalListeners();
+view.setupNewTaskSubmitListener(addTask);
+view.setupNewProjectModalListeners();
+view.setupNewProjectSubmitListener(addProject);
+view.setupProjectViewSelectListener(getAllTasksInProject);
+view.setupProjectSelectListener(getAllTasksInProject);
+
+function getProjectTitles() {
+  return Object.keys(projects);
+}
 
 projects["Default List"] = [];
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  let sampleTodoItem = new TodoItem("eat", "food", "2024-06-20", 2, "nachos");
-  addToDoItem(sampleTodoItem, "Default List");
-  console.log(projects);
-  const projectViewSelect = document.querySelector("#project-view-select");
-  populateProjectsIntoSelect(projectViewSelect);
+  let sampleTask = new Task("eat", "food", "2024-06-20", 2, "nachos");
+  addTask(sampleTask, "Default List");
+  view.populateProjectsIntoSelects(getProjectTitles());
 });
 
-
-function addToDoItem(newTodoItem, project) {
-  if (!(project in projects)) {
-    throw new Error(`project with name '${project}' does not exist in projects.`)
+//separated from view
+function addTask(newTask, projectTitle) {
+  if (!(projectTitle in projects)) {
+    throw new Error(`project with name '${projectTitle}' does not exist in projects.`)
   }
-  projects[project].push(newTodoItem);
-  console.log(projects);
-  displayAllTasksInProject(project);
+  projects[projectTitle].push(newTask);
+  view.displayTasks(getAllTasksInProject(projectTitle));
+  view.updateProjectTitle(projectTitle);
 }
 
-
-const newTaskModal = document.querySelector('#new-task-modal');
-const newTaskButton = document.querySelector('#new-task-button');
-const closeNewTaskModalSpan = document.querySelector("#close-new-task-span");
-const newTaskForm = document.querySelector('#new-task-form');
-const projectSelect = document.querySelector('#project-select');
-
-newTaskButton.addEventListener('click', () => {
-    newTaskModal.style.display = "block";
-    populateProjectsIntoSelect(projectSelect);
-    projectSelect.value = projectViewSelect.value;
-});
-
-function populateProjectsIntoSelect(select) {
-  select.innerHTML = '';
-    for (const [key] of Object.entries(projects)) {
-      const projectOption = document.createElement('option');
-      projectOption.innerHTML = key;
-      select.appendChild(projectOption);
-    }
-}
-
-window.onclick = function(event) {
-    if (event.target == newTaskModal) {
-        closeAndResetTaskModal();
-    }
-}
-
-closeNewTaskModalSpan.addEventListener('click', () => {
-  closeAndResetTaskModal();
-})
-
-newTaskForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    // Retrieve form data
-    const formData = new FormData(newTaskForm);
-    const taskTitle = formData.get('title');
-    const taskDescription = formData.get('description');
-    const taskDueDate = formData.get('due-date');
-    const taskPriority = formData.get('priority');
-    const taskNotes = formData.get('notes');
-    const taskProject = formData.get('project');
-
-    const newTask = new TodoItem(taskTitle, taskDescription, taskDueDate, taskPriority, taskNotes);
-    console.log(newTask);
-
-    addToDoItem(newTask, taskProject);
-
-    closeAndResetTaskModal();
-})
-
-function closeAndResetTaskModal() {
-    newTaskModal.style.display = "none";
-    newTaskForm.reset();
-}
-
-const mainContent = document.querySelector('#main-content');
-
-function displayAllTasksInProject(project) {
-  mainContent.innerHTML = '';
-  const currentProject = projects[project];
-  console.log(currentProject)
-  currentProject.forEach((task) => {
-    const taskDisplayElement = createTaskDisplayElement(task);
-    mainContent.appendChild(taskDisplayElement);
-  });
-  const projectTitle = document.querySelector('#project-title');
-  projectTitle.innerHTML = projectViewSelect.value;
-}
-
-function createTaskDisplayElement(task) {
-  const taskDisplayDiv = document.createElement('div');
-  const taskDisplayTitle = document.createElement('h3');
-  const taskDisplayDescription = document.createElement('p');
-  const taskDisplayDueDate = document.createElement('p');
-  const taskDisplayNotes = document.createElement('p');
-
-  taskDisplayTitle.innerHTML = task.title;
-  taskDisplayDescription.innerHTML = task.description;
-  taskDisplayDueDate.innerHTML = `Due: ${task.dueDate}`;
-  taskDisplayNotes.innerHTML = task.notes;
-
-  taskDisplayDiv.appendChild(taskDisplayTitle);
-  taskDisplayDiv.appendChild(taskDisplayDueDate);
-  taskDisplayDiv.appendChild(taskDisplayDescription);
-  taskDisplayDiv.appendChild(taskDisplayNotes);
-
-  switch (task.priority) {
-    case 1: taskDisplayDiv.classList.add('low-priority'); break;
-    case 2: taskDisplayDiv.classList.add('med-priority'); break;
-    default: taskDisplayDiv.classList.add('high-priority');
-  }
-
-  taskDisplayDiv.classList.add('task-display');
-
-  return taskDisplayDiv;
-}
-
-
-
-
-const newProjectModal = document.querySelector('#new-project-modal');
-const newProjectButton = document.querySelector('#new-project-button');
-const closeNewProjectModalSpan = document.querySelector("#close-new-project-span");
-const newProjectForm = document.querySelector('#new-project-form');
-
-newProjectButton.addEventListener('click', () => {
-  newProjectModal.style.display = "block";
-});
-
-function closeAndResetProjectModal() {
-  newProjectModal.style.display = "none";
-  newProjectForm.reset();
-}
-
-window.onclick = function(event) {
-  if (event.target == newProjectModal) {
-      closeAndResetProjectModal();
-  }
-}
-
-closeNewProjectModalSpan.addEventListener('click', () => {
-closeAndResetProjectModal();
-})
-
-newProjectForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  // Retrieve form data
-  const formData = new FormData(newProjectForm);
-  const projectTitle = formData.get('title');
-
-  console.log(projectTitle);
-
+function addProject(projectTitle) {
   projects[projectTitle] = [];
+  view.populateProjectsIntoSelects(getProjectTitles());
+  return getAllTasksInProject(projectTitle);
+}
 
-  const projectViewSelect = document.querySelector("#project-view-select");
-  populateProjectsIntoSelect(projectViewSelect);
-  populateProjectsIntoSelect(projectSelect);
-
-  closeAndResetProjectModal();
-});
-
-const projectViewSelect = document.querySelector('#project-view-select')
-
-
-projectViewSelect.addEventListener('change', (event) => {
-  displayAllTasksInProject(projectViewSelect.value);
-})
+function getAllTasksInProject(project) {
+  return projects[project]
+}
---
+++ src/view.js
@@ -0,0 +1,176 @@
+import { Task } from './task.js';
+
+export class View {
+  constructor() {
+    this.mainContent = document.querySelector("#main-content");
+    this.projectTitle = document.querySelector("#project-title");
+    this.projectViewSelect = document.querySelector("#project-view-select");
+
+    this.newTaskButton = document.querySelector("#new-task-button");
+    this.newTaskModal = document.querySelector("#new-task-modal");
+    this.closeNewTaskModalSpan = document.querySelector("#close-new-task-span");
+    this.newTaskForm = document.querySelector("#new-task-form");
+    this.projectSelect = document.querySelector("#project-select");
+    
+    this.newProjectButton = document.querySelector("#new-project-button");
+    this.newProjectModal = document.querySelector("#new-project-modal");
+    this.closeNewProjectModalSpan = document.querySelector("#close-new-project-span");
+    this.newProjectForm = document.querySelector("#new-project-form");
+  }
+
+  displayTasks(tasks) {
+    this.mainContent.innerHTML = "";
+    tasks.forEach((task) => {
+      const taskDisplayElement = this.createTaskDisplayElement(task);
+      this.mainContent.appendChild(taskDisplayElement);
+    });
+  }
+
+  updateProjectTitle(title) {
+    this.projectTitle.innerHTML = title;
+    this.updateProjectSelects(title);
+  }
+
+  createTaskDisplayElement(task) {
+    const taskDisplayDiv = document.createElement("div");
+    const taskDisplayTitle = document.createElement("h3");
+    const taskDisplayDescription = document.createElement("p");
+    const taskDisplayDueDate = document.createElement("p");
+    const taskDisplayNotes = document.createElement("p");
+
+    taskDisplayTitle.innerHTML = task.title;
+    taskDisplayDescription.innerHTML = task.description;
+    taskDisplayDueDate.innerHTML = `Due: ${task.dueDate}`;
+    taskDisplayNotes.innerHTML = task.notes;
+
+    taskDisplayDiv.appendChild(taskDisplayTitle);
+    taskDisplayDiv.appendChild(taskDisplayDueDate);
+    taskDisplayDiv.appendChild(taskDisplayDescription);
+    taskDisplayDiv.appendChild(taskDisplayNotes);
+
+    switch (Number(task.priority)) {
+      case 1:
+        taskDisplayDiv.classList.add("low-priority");
+        break;
+      case 2:
+        taskDisplayDiv.classList.add("med-priority");
+        break;
+      case 3:
+        taskDisplayDiv.classList.add("high-priority");
+    }
+
+    taskDisplayDiv.classList.add("task-display");
+
+    return taskDisplayDiv;
+  }
+
+  setupNewTaskModalListeners() {
+    this.newTaskButton.addEventListener("click", () => {
+      this.newTaskModal.style.display = "block";
+      this.projectSelect.value = this.projectViewSelect.value;
+    });
+
+    this.closeNewTaskModalSpan.addEventListener("click", () => {
+      this.closeAndResetTaskModal();
+    });
+  }
+
+  setupNewTaskSubmitListener(callback) {
+    this.newTaskForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      // Retrieve form data
+      const formData = new FormData(this.newTaskForm);
+      const taskTitle = formData.get("title");
+      const taskDescription = formData.get("description");
+      const taskDueDate = formData.get("due-date");
+      const taskPriority = formData.get("priority");
+      const taskNotes = formData.get("notes");
+      const taskProject = formData.get("project");
+
+      const newTask = new Task(
+        taskTitle,
+        taskDescription,
+        taskDueDate,
+        taskPriority,
+        taskNotes
+      );
+      callback(newTask, taskProject);
+
+      this.closeAndResetTaskModal();
+    });
+  }
+
+  closeAndResetTaskModal() {
+    this.newTaskModal.style.display = "none";
+    this.newTaskForm.reset();
+  }
+
+  setupNewProjectModalListeners() {
+    this.closeNewProjectModalSpan.addEventListener("click", () => {
+      this.closeAndResetProjectModal();
+    });
+
+    this.newProjectButton.addEventListener("click", () => {
+      this.newProjectModal.style.display = "block";
+    });
+  }
+
+  setupNewProjectSubmitListener(addProjectCallback) {
+    this.newProjectForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      // Retrieve form data
+      const formData = new FormData(this.newProjectForm);
+      const projectTitle = formData.get("title");
+      addProjectCallback(projectTitle);
+
+      this.displayTasks(addProjectCallback(projectTitle));
+      this.updateProjectTitle(projectTitle);
+
+      this.closeAndResetProjectModal();
+    });
+  }
+
+  closeAndResetProjectModal() {
+    this.newProjectModal.style.display = "none";
+    this.newProjectForm.reset();
+  }
+
+  populateProjectsIntoSelects(projectTitles) {
+    this.projectViewSelect.innerHTML = "";
+    this.projectSelect.innerHTML = "";
+    projectTitles.forEach((projectTitle) => {
+      const projectOption = document.createElement("option");
+      const projectViewOption = document.createElement("option");
+
+      projectOption.innerHTML = projectTitle;
+      projectViewOption.innerHTML = projectTitle;
+
+      this.projectSelect.appendChild(projectOption);
+      this.projectViewSelect.appendChild(projectViewOption);
+    });
+  }
+
+  updateProjectSelects(projectTitle) {
+    this.projectViewSelect.value = projectTitle;
+    this.projectSelect.value = projectTitle;
+  }
+
+  setupProjectViewSelectListener(getAllTasksInProjectCallback) {
+    this.projectViewSelect.addEventListener("change", (event) => {
+      const projectTitle = this.projectViewSelect.value;
+      this.displayTasks(getAllTasksInProjectCallback(projectTitle));
+      this.updateProjectTitle(projectTitle);
+    });
+  }
+
+  setupProjectSelectListener(getAllTasksInProjectCallback) {
+    this.projectSelect.addEventListener("change", (event) => {
+      const projectTitle = this.projectSelect.value;
+      this.displayTasks(getAllTasksInProjectCallback(projectTitle));
+      this.updateProjectTitle(projectTitle);
+    });
+  }
+
+}
